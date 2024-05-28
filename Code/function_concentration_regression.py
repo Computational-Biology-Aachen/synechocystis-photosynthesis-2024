07fed313de8b00e31fd7e879210c8eb29b285156
Tobias Pfennig <tobiaspfennig@gmx.de>
2024-05-28 20:27:32 +0200

    added the biomass function to the model

--- Code/function_concentration_regression.py
+++ Code/function_concentration_regression.py
@@ -33,9 +33,10 @@ from datetime import datetime
 from SMTPMailSender import SMTPMailSender
 
 # Set 
-max_workers = 100 # os.cpu_count() - 2
+max_workers = 100
+max_workers = np.min([max_workers, os.cpu_count() - 2])
 file_prefix = f"concregression_{datetime.now().strftime('%Y%m%d%H%M')}"
-n_points = 10
+n_points = 3
 
 # Setup the email sender
 email = SMTPMailSender(
@@ -116,17 +117,72 @@ def make_light_into_input(m, y0={}, init_param=None, verbose=True):
     # m = sort_algmodules(m)
     return m, y0
 
-# %%
-m, y0 = get_model(
-    check_consistency=False,
-    verbose=False,
+# Get the model and add the biomass reaction
+# Get the default model
+_m = get_model(get_y0=False, verbose=False, check_consistency=False)
+
+stoich_3PGA = pd.Series({
+    "CO2": -3,
+    "ATP": -8,
+    "NADPH": -5,
+    "3PGA": 1,
+})
+
+# Biomass stoichiometry as given in Knoop2015
+# This reaction has to complete once for a full division
+biomass_Knoop2015 = pd.Series({ # [mmol gDW(Biomass)^-1]
+    "NADPH": -90, 
+    "ATP": -191,
+    "CO2": -41.5,
+    
+    "3PGA": 0, # Added for compliance with the model
+})
+
+# Transform to usage of 3PGA
+biomass_Knoop2015 = biomass_Knoop2015 - (stoich_3PGA / stoich_3PGA["CO2"] * biomass_Knoop2015["CO2"])
+biomass_Knoop2015 = biomass_Knoop2015.drop("CO2")
+biomass_Knoop2015["Ho"] = biomass_Knoop2015["NADPH"] / _m.get_parameter("bHo")
+
+# Calculate the conversion from gDW to mol(Chl)
+mChl_DW = 10 # [mg gDW^-1] (Zavrel2019)
+MChl = 893.509 # [g mol^-1]
+
+nChl_DW = ( # [mol gDW^-1]
+    mChl_DW 
+    * 1e-3 # [g mg^-1]
+    / MChl # [g mol^-1]
+)
+
+# Scale the biomass stoichiometry to mol Chl
+biomass_Knoop2015 = biomass_Knoop2015 / nChl_DW # [mmol mol(Chl)^-1]
+
+
+# Get the growth rate of the 633nm (red)-light grown cells
+growth_rate= pd.read_csv("../Code/data/Strainparameters_Zavrel2021.csv", header=0,skiprows=1, index_col=0).loc["Specific growth rate","633"] # [h^-1]
+growth_rate = growth_rate / 3600 # [s^-1]
+# Get the default model
+m, y0 = get_model(verbose=False, check_consistency=False)
+
+# Set the initial 3PGA concentration to zero
+y0["3PGA"] = 0
+
+# Add a biomass reaction as Mass action
+biomass_stoich = biomass_Knoop2015.to_dict()
+
+# Add parameter(s) and reaction to model
+m.add_parameter("k_biomass", growth_rate * 10)
+m.add_reaction_from_args(
+    "vBiomass",
+    function = rf.mass_action_variable,
+    stoichiometry = biomass_stoich,
+    args = ["k_biomass"] + list(biomass_stoich.keys()),
 )
 
 m, y0 = make_light_into_input(m, y0)
 
 # %%
 # Generate the input light data
-_light_input = np.linspace(10, 20000, n_points)
+_light_input = np.linspace(10, 1000, n_points)
 
 light_input = np.array(np.meshgrid(
     _light_input,
@@ -159,6 +215,14 @@ def get_outputs(x, p_keys, m, y0, compounds=["ATP", "NADPH", "3PGA", "Fd_red"], 
 
     # print(index, p)
 
+    integrator_kwargs = {
+        "maxsteps": 20000,
+        "atol": 1e-9,
+        "rtol": 1e-9,
+        "maxnef": 10,
+        "maxncf": 10,
+    }
+
     # Simulate to steady state
     s, t, y = simulate_to_steady_state_custom(
         s,
@@ -169,7 +233,7 @@ def get_outputs(x, p_keys, m, y0, compounds=["ATP", "NADPH", "3PGA", "Fd_red"], 
         },
         rel_norm=True,
         return_simulator=True,
-        **fnc.simulator_kwargs["loose"],
+        **integrator_kwargs,
     )
 
     # Get the concentrations 
@@ -208,12 +272,13 @@ with warnings.catch_warnings():
         ))
 
 result = pd.concat(res, axis=1).T.reset_index().drop("index", axis=1)
+n_successful = n_points-result.isna().any(axis=1).sum()
 
 # Save the parameters and results
 light_input.to_csv(Path(f"../Results/{file_prefix}_params.csv",))
 result.to_csv(Path(f"../Results/{file_prefix}_results.csv",))
 
 email.send_email(
-    body=f"Regression run {file_prefix} was successfully finished",
+    body=f"Regression run {file_prefix} was successfully finished\n{n_successful} simulations were successful",
     subject="Regression finished"
 )
